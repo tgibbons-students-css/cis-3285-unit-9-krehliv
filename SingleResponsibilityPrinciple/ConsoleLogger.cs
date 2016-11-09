7bc6e8d66d20689ce5c9baa0bf26242acf0887ca
Krehl Stringer <kstringer@css.edu>
2016-11-08 20:25:54 -0600

    Request 405: Monitor trading errors

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using SingleResponsibilityPrinciple.Contracts;
+using System.IO;
 
 namespace SingleResponsibilityPrinciple
 {
@@ -9,6 +10,16 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><message>" + message + "</message></log>", args);
+            }
+        }
+
+        private void LogMessage(string type, string message, params object[] args)
+        {
+            Console.WriteLine(message, args);
+
         }
 
         public void LogInfo(string message, params object[] args)
